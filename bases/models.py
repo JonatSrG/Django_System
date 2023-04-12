66c9e055d7260bc931a5dbb43154f3ffadc84812
JonatSrG <JonatSrG>
2023-04-12 13:22:33 -0600

    Model creado categoria

--- bases/models.py
+++ bases/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 class ClaseModelo(models.Model):
     estado = models.BooleanField(default=True)
-    fc = models.DateTiemField(auto_now_add=True)
-    fm = models.DateTiemField(auto_now=True)
+    fc = models.DateTimeField(auto_now_add=True)
+    fm = models.DateTimeField(auto_now=True)
     uc = models.ForeignKey(User, on_delete=models.CASCADE)
-    um = models.IntergerField(blank=True,null=True)
+    um = models.IntegerField(blank=True,null=True)
 
     class Meta:
         abstract=True
