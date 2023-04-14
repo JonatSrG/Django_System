01d59ff6e82a8d41d4ee94ce3631e2054551e5f3
JonatSrG <JonatSrG>
2023-04-14 14:04:10 -0600

    crud completo de marca

--- inv/views.py
+++ inv/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Categoria, SubCategoria
-from .forms import CategoriaForm, SubCategoriaForm
+from .models import Categoria, SubCategoria, Marca
+from .forms import CategoriaForm, SubCategoriaForm, MarcaForm
 
 
 class CategoriaView(LoginRequiredMixin, generic.ListView):
@@ -90,3 +90,62 @@ class SubCategoriaDel(LoginRequiredMixin, generic.DeleteView):
     success_url=reverse_lazy("inv:subcategoria_list")
     """ success_message="Sub Categoría Eliminada"
     permission_required="inv.delete_subcategoria" """
+
+
+class MarcaView(LoginRequiredMixin, generic.ListView):
+    """ permission_required = "inv.view_marca" """
+    model = Marca
+    template_name = "inv/marca_list.html"
+    context_object_name = "obj"
+    login_url = 'bases:login'
+
+
+class MarcaNew(LoginRequiredMixin, generic.CreateView):
+    model=Marca
+    template_name="inv/marca_form.html"
+    context_object_name = 'obj'
+    form_class=MarcaForm
+    success_url= reverse_lazy("inv:marca_list")
+    """ success_message="Marca Creada"
+    permission_required="inv.add_marca" """
+    login_url = 'bases:login'
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+
+
+class MarcaEdit(LoginRequiredMixin, generic.UpdateView):
+    model=Marca
+    template_name="inv/marca_form.html"
+    context_object_name = 'obj'
+    form_class=MarcaForm
+    success_url= reverse_lazy("inv:marca_list")
+    """ success_message="Marca Editada"
+    permission_required="inv.change_marca" """
+    login_url = 'bases:login'
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+
+
+def marca_inactivar(request, id):
+    marca = Marca.objects.filter(pk=id).first()
+    contexto={}
+    template_name="inv/catalogos_del.html"
+
+
+    if not marca:
+        return redirect("inv:marca_list")
+    
+    if request.method=='GET':
+        contexto={'obj':marca}
+    
+    if request.method=='POST':
+        marca.estado=False
+        marca.save()
+        """ messages.success(request, 'Marca Inactivada') """
+        return redirect("inv:marca_list")
+
+    return render(request,template_name,contexto)
